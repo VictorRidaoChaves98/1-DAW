99967cda749042d4aaf97f8537c6b978360faade
Victor Ridao Chaves <victormanuel.ridaochaves@adaits.es>
2025-03-19 19:08:20 +0100

    .

---
+++ programacion/t2/java/ExamenRepasoStarWars/Jedi.java
@@ -0,0 +1,10 @@
+
+import java.util.HashMap;
+
+public class Jedi {
+    
+    public int nivelFuerza;
+    HashMap<String, Integer> habilidadesEspeciales;
+    ArrayList<Transporte> naves;
+    
+}
